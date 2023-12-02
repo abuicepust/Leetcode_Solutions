b073c303b81ea7ef32990880793ff54033bd4565
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-12-02 10:34:13 +0600

    Add files via upload

---
+++ ClimbingStairs2.java
@@ -0,0 +1,18 @@
+public class ClimbingStairs2 {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(6));
+    }
+    public static int climbStairs(int n) {
+     
+        if(n<=3){
+            return n;
+        }
+        int a=3;int b=2;
+        for(int i=0;i<n-3;i++){
+            a = a+b;
+            b = a-b;
+        }
+        return a;
+
+    }
+}

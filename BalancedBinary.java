79ba9619c8b38b2f856302c6d7f23fbc9031e84b
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-12-13 14:42:16 +0600

    Add files via upload
    Balanced Binary Tree solution

---
+++ BalancedBinary.java
@@ -0,0 +1,21 @@
+public class BalancedBinary {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(20);
+        System.out.println(isBalanced(root));
+    }
+    public static boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+
+        if (Math.abs(height(root.left) - height(root.right)) > 1) return false;
+
+        return isBalanced(root.left) && isBalanced(root.right);
+    }
+    public static int height(TreeNode node){
+        if (node == null) return 0;
+        return 1 + Math.max(height(node.left), height(node.right));
+    }
+}

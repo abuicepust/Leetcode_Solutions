f3ccaa373d30fd1de8bafce26b48bd1442d15a5a
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-12-19 17:43:08 +0600

    Add files via upload
    Single Number solution.

---
+++ SingleNumber.java
@@ -0,0 +1,13 @@
+public class SingleNumber {
+    public static void main(String[] args) {
+       int[] nums = {2,2,1};
+       System.out.println(singleNumber(nums)); 
+    }
+    public static int singleNumber(int[] nums) {
+     int value = 0;
+     for(int i=0; i<nums.length;i++){
+        value = value ^ nums[i];
+     }  
+     return value;
+    }
+}

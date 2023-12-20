e85d642ba004c6f5636ca5f0076b045b73c6452d
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-12-20 20:55:59 +0600

    Add files via upload
    Binary Tree Preorder Traversal Solution.

---
+++ BinaryPre_Order.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryPre_Order {
+    public static void main(String[] args) {
+        
+    }
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorder(result, root);
+        
+        return preorder;
+    }
+    public static void preorder(List<Integer> result, TreeNode root){
+        
+    }
+}

4ca273f4d0965598b257a5472a5bff61313f440c
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-12-21 11:33:29 +0600

    Add files via upload
    Invert Binary Tree Solution.

---
+++ InverBi_Tree.java
@@ -0,0 +1,19 @@
+public class InverBi_Tree {
+    public static void main(String[] args) {
+        
+    }
+    public static TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.left = right;
+        root.right = left;
+
+        return root;
+    }
+
+}
